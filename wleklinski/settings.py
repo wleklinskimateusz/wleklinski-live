dc058278b7a3a5e463849a69521968630e9f1e8c
matix0508 <matix0508@gmail.com>
2020-05-01 14:16:49 +0200

    next

--- wleklinski/settings.py
+++ wleklinski/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v*8ywsed#lkp_ubu5d9m#xs9nnteonjh#zw_k-hom+u*agtoe='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wleklinski.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
